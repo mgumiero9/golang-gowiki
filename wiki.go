817499c6c9f0b805bd0a55ad480035c6d124b17b
mgumiero9 <mgumiero9@gmail.com>
2016-12-10 20:28:43 -0200

    Adding func loadPage

--- wiki.go
+++ wiki.go
@@ -4,9 +4,7 @@ import (
     "fmt"
     "io/ioutil"
 )
-/**
-* Page definition
-*/
+
 type Page struct {
     Title string
     Body []byte
@@ -15,4 +13,10 @@ type Page struct {
 func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+    filename := title + ".txt"
+    body, _ := ioutil.ReadFile(filename)
+    return &Page{Title: title, Body: body}
 }
